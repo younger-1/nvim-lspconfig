819a3358b1e127c13af1cc80ad19d5aaabb9defc
Xavier Young <45989017+younger-1@users.noreply.github.com>
2022-04-27 13:33:44 +0800

    Update luahelper.lua

--- lua/lspconfig/server_configurations/luahelper.lua
+++ lua/lspconfig/server_configurations/luahelper.lua
@@ -7,13 +7,24 @@ if vim.fn.has 'unix' == 1 then
 else if vim.fn.has 'mac' == 1 then
   bin_name = 'mac' .. bin_name
 end
+  
+local root_files = {
+  '.luarc.json',
+  '.luacheckrc',
+  '.stylua.toml',
+  'selene.toml',
+  'luahelper.json',
+}
 
 return {
   default_config = {
     cmd = { bin_name, '-mode', '1' },
     filetypes = { 'lua' },
-    root_dir = util.find_git_ancestor,
+    root_dir = function(fname)
+      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+    end,
     single_file_support = true,
+    log_level = vim.lsp.protocol.MessageType.Warning,
   },
   docs = {
     package_json = 'https://raw.githubusercontent.com/Tencent/LuaHelper/master/luahelper-vscode/package.json',
@@ -21,6 +32,11 @@ return {
 https://github.com/Tencent/LuaHelper
 
 Lua language server.
+
+The binary of `luahelper` for Windows/Linux/Mac can be found [here](https://github.com/Tencent/LuaHelper/tree/master/luahelper-vscode).
+The default `cmd` assumes that the `luahelper`'s binary can be found in `$PATH`.
+
+`luahelper` uses [LuaFormatter](https://github.com/Koihik/LuaFormatter) to format code by default.
     ]],
     default_config = {
       root_dir = [[root_pattern(".git") or bufdir]],
